752b4fc3ab2e6f70a9163b1f8eace691af111efb
anoobsuresh04 <anoob.suresh@perleybrook.com>
2024-03-18 14:16:08 +0530

    Add "All" category to show first

--- nextjs-blog/src/components/Categories.tsx
+++ nextjs-blog/src/components/Categories.tsx
@@ -10,13 +10,20 @@ const Categories = ({ categories }: { categories: any }) => {
     changeCategory(categories?.data[2].attributes.Title);
   }, [categories]);
 
-  // Sort the categories in ascending order
-  const sortedCategories = categories?.data?.sort((a: any, b: any) =>
-    a.attributes.Title.localeCompare(b.attributes.Title)
-  );
+  // Separate the categories into two arrays: allCategories and sortedCategories
+  const allCategories = categories?.data?.filter((category: any) => category.attributes.Title === "All");
+  const sortedCategories = categories?.data?.filter((category: any) => category.attributes.Title !== "All")
+    .sort((a: any, b: any) => a.attributes.Title.localeCompare(b.attributes.Title));
 
   return (
     <div className="flex gap-6 mb-8 ">
+      {/* Display all categories first */}
+      {allCategories?.map((category: any) => (
+        <div key={category.id}>
+          <Category cat={category} />
+        </div>
+      ))}
+      {/* Display sorted categories */}
       {sortedCategories?.map((category: any) => (
         <div key={category.id}>
           <Category cat={category} />
