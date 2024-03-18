be9c1d37530ba50b8efb0d6ad8aa895f7e03030d
anoobsuresh04 <anoob.suresh@perleybrook.com>
2024-03-18 14:12:35 +0530

    Add sorting to cateogies to sort data from all to others

--- nextjs-blog/src/components/Categories.tsx
+++ nextjs-blog/src/components/Categories.tsx
@@ -4,18 +4,20 @@ import Category from "./Category";
 import { CategoryContext } from "@/context/CategoryContext";
 
 const Categories = ({ categories }: { categories: any }) => {
-
-  const {changeCategory} = useContext(CategoryContext);
+  const { changeCategory } = useContext(CategoryContext);
 
   useLayoutEffect(() => {
     changeCategory(categories?.data[2].attributes.Title);
-  } 
-  , [categories]);
+  }, [categories]);
 
+  // Sort the categories in ascending order
+  const sortedCategories = categories?.data?.sort((a: any, b: any) =>
+    a.attributes.Title.localeCompare(b.attributes.Title)
+  );
 
   return (
     <div className="flex gap-6 mb-8 ">
-      {categories?.data?.map((category: any) => (
+      {sortedCategories?.map((category: any) => (
         <div key={category.id}>
           <Category cat={category} />
         </div>
